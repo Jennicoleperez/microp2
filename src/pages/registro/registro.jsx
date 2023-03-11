ac6ab3b3da0cd68ba4fd504792036706bdc1d23b
Jennifer Pérez <jennifer.perez@correo.unimet.edu.ve>
2023-03-11 16:16:32 -0400

    .

--- src/pages/registro/registro.jsx
+++ src/pages/registro/registro.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import Style from './registro.module.css'
+import collage from "../../image/collage.jpg"
 
 function Registro() {
   return (
     <div>
     
     <h1>Registro de usuario</h1>
-  
+    <section>
+      <div className="back_image">
+        <img src={collage} alt="Collage de películas" />
+      </div>
+    </section>
+    
+   
+    
     
     
     </div>
