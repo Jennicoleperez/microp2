6cc1683bc3471eb620f14bb4d65a144ac9d7d72f
Jennifer Pérez <jennifer.perez@correo.unimet.edu.ve>
2023-03-11 21:52:47 -0400

    Registro

--- src/pages/registro/registro.jsx
+++ src/pages/registro/registro.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import collage1 from "../../image/collage1.jpg"
 import Style from "../registro/registro.module.css"
+import logo from '../../image/logo.svg'
+import {Link} from 'react-router-dom';
+
+const LOGIN_URL="";
 
 function Registro() {
   return (
@@ -9,13 +13,32 @@ function Registro() {
       <div className={Style.fondito1}>
         <img src={collage1} alt="Collage de películas" />
       </div>
-      
-
-      <h1 className={Style.titulo}>Registro de usuario</h1>
-
-      <div className={Style.formulario}>
-
+      <div className={Style.logo}>
+        <img src={logo} alt="Collage de películas" />
       </div>
+      
+      <button className={Style.ingresar}>
+        Iniciar Sesión
+      </button>
+      
+        <form className={Style.formulario}>
+          <h2 className={Style.titulo}>Crea tu cuenta aquí</h2>
+          <div className={Style.subt1}>
+            <label>Nombre:</label>
+            <input type="text" id="firstName" />
+          </div>
+          <div  className={Style.subt2}>
+            <label>Apellido:</label>
+            <input type="text" id="lastName" />
+          </div>
+          <div  className={Style.subt3}>
+            <label>Email:</label>
+            <input type="email" id="email" />
+          </div>
+
+        </form>
+      
+      
 
     
    
