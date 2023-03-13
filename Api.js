7bc6fdb3a3d7c81563b6941c2e822b6fc5fe9ec1
Jesus Perez <jesuspereza3108@gmail.com>
2023-03-12 19:54:07 -0700

    crear funcion cargarP

--- Api.js
+++ Api.js
@@ -1,12 +1,7 @@
-const APIkey = "90f27e7fca27aa224261ee839d68b94b"
-const mostrarD = document.getElementById('detalles')
+const cargarP = async() => {
+    const response  = await fetch("https://api.themoviedb.org/3/movie/550?api_key=90f27e7fca27aa224261ee839d68b94b");
 
-mostrarD = addEventListener('click', async () => {
-    try {
-        const response = await fetch{
-            'https://api.themoviedb.org/3/movie/550?api_key=${APIkey}'
-        };
-    } catch (error) {
-        console.log("ERROR", error);
-    };
-})
+    console.log(response);
+}
+
+cargarP();
