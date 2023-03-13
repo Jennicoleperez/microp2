a403fb0075fd8f7ce6ad6195356c47f2425d644a
Jesus Perez <jesuspereza3108@gmail.com>
2023-03-12 22:09:29 -0700

    cambios mayores y reseteo

--- Api.js
+++
@@ -1,7 +0,0 @@
-const cargarP = async() => {
-    const response  = await fetch("https://api.themoviedb.org/3/movie/550?api_key=90f27e7fca27aa224261ee839d68b94b");
-
-    console.log(response);
-}
-
-cargarP();
