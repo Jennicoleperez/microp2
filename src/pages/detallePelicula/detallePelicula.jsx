b229921fca9b150f025917fc43fe4464568bcce2
Jesus Perez <jesuspereza3108@gmail.com>
2023-03-13 01:02:56 -0700

    cambios finales

--- src/pages/detallePelicula/detallePelicula.jsx
+++ src/pages/detallePelicula/detallePelicula.jsx
@@ -1,37 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react';
+import Style from './detallePelicula.module.css';
 
-const keyAPI = '90f27e7fca27aa224261ee839d68b94b'
+const key = '90f27e7fca27aa224261ee839d68b94b';
 
 const cargarP = async() => {
-
   try {
-      const response  = await fetch(`https://api.themoviedb.org/3/movie/315162?api_key=${keyAPI}`);
+    const results = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${key}`);
 
-      console.log(response);
+console.log(results);
 
-      const data = await response.json();
+const data = await results.json();
 
-      let films = ``;
-      films = `<h1>${data.title}</h1> <h2>${data.id}</h2> <h2>${data.overview}</h2>
-      <h2><img src='${data.poster_path}' alt='${data.title}'></h2>`
+let films = `
+  <div>
+    <h1 className='titulo'>Detalle de {data.title}</h1><br></br>
+    <img src={data.poster_path} alt={data.title} /><br></br>
+    <h2 className='subtitulo'>Description: {data.overview}</h2><br></br>
+    <h2 className='subtitulo'>Original laguage: {data.original_language}</h2><br></br>
+    <h2 className='subtitulo'>Budget: {data.budget}</h2><br></br>
+    <h2 className='subtitulo'>Popularity: {data.popularity}</h2><br></br>
+    <h2 className='subtitulo'>Release date: {data.release_date}</h2><br></br>
+    <h2 className='subtitulo'>Status: {data.status}</h2><br></br>
+    <h2 className='subtitulo'>Detalle de {data.title}</h2><br></br><br></br>
+  </div>`
 
-      document.getElementById('root').innerHTML = films;
-
+document.getElementById('detail').innerHTML = films;
 
   } catch (error) {
-      console.log(error);
+    alert('Pelicula no encontrada')
   }
-
 }
 
 cargarP();
 
 function DetallePelicula() {
   return (
-    <div>DetallePelicula
-      <h1></h1>
+    <div className={Style.cuadro} id='detail'>
+
     </div>
+
   )
 }
 
