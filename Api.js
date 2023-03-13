cf0acedd8444e812800c3271819d9f6dc60b3102
Jesus Perez <jesuspereza3108@gmail.com>
2023-03-12 18:18:14 -0700

    modificar Api.js

--- Api.js
+++ Api.js
@@ -1,9 +1,12 @@
-const APIkey = "90f27e7fca27aa224261ee839d68b94b";
+const APIkey = "90f27e7fca27aa224261ee839d68b94b"
+const mostrarD = document.getElementById('detalles')
 
-try {
-    const response = fetch{
-        'https://api.themoviedb.org/3/movie/550?api_key=${APIkey}'
+mostrarD = addEventListener('click', async () => {
+    try {
+        const response = await fetch{
+            'https://api.themoviedb.org/3/movie/550?api_key=${APIkey}'
+        };
+    } catch (error) {
+        console.log("ERROR", error);
     };
-} catch (error) {
-    console.log("ERROR", error);
-};
+})
