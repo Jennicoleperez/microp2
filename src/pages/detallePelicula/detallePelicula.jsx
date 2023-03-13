02498dc282a717f59fb458617129b0b6d9a352b1
Jesus Perez <jesuspereza3108@gmail.com>
2023-03-12 23:35:46 -0700

    restaurar index

--- src/pages/detallePelicula/detallePelicula.jsx
+++ src/pages/detallePelicula/detallePelicula.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import ReactDOM from 'react';
 
+const keyAPI = '90f27e7fca27aa224261ee839d68b94b'
+
+const cargarP = async() => {
+
+  try {
+      const response  = await fetch(`https://api.themoviedb.org/3/movie/315162?api_key=${keyAPI}`);
+
+      console.log(response);
+
+      const data = await response.json();
+
+      let films = ``;
+      films = `<h1>${data.title}</h1> <h2>${data.id}</h2> <h2>${data.overview}</h2>
+      <h2><img src='${data.poster_path}' alt='${data.title}'></h2>`
+
+      document.getElementById('root').innerHTML = films;
+
+
+  } catch (error) {
+      console.log(error);
+  }
+
+}
+
+cargarP();
 
 function DetallePelicula() {
   return (
-    <div>DetallePelicula</div>
+    <div>DetallePelicula
+      <h1></h1>
+    </div>
   )
 }
 
